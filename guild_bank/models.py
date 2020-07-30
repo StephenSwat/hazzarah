432c9894401b8e025c551cddd18c664ea21b7b33
Stephen Nicholas Swatman <stephen@v25.nl>
2020-07-31 01:21:36 +0200

    Add some very simple guild bank models

--- guild_bank/models.py
+++ guild_bank/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+
+class Scan(models.Model):
+    character = models.ForeignKey(Character, models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class ScanItem(models.Model):
+    scan = models.ForeignKey(Scan, models.CASCADE)
+    item = models.ForeignKey(Item, models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        unique_together = ('scan', 'item',)
