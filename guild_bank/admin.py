a3a667c81d968797a10d14a4de8344c4230ca4eb
Stephen Nicholas Swatman <stephen@v25.nl>
2020-07-31 02:58:59 +0200

    Add our models to the admin interface

--- guild_bank/admin.py
+++ guild_bank/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from guild_bank.models import Character, Item, Scan, ScanItem
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Scan)
+class ScanAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ScanItem)
+class ScanItemAdmin(admin.ModelAdmin):
+    pass
