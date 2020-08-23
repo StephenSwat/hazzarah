2b3311050b681e22a5140ba6928813bd5094de94
Stephen Nicholas Swatman <stephen@v25.nl>
2020-08-23 15:22:08 +0200

    Add a model for the bank

--- guild_bank/models.py
+++ guild_bank/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class Bank(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+
 class Item(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
